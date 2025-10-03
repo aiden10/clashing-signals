a3aedfe845a3794b4dc78caa1a4f2871127cadea
ilikeapple11 <133582503+ilikeapple11@users.noreply.github.com>
2025-10-03 18:31:44 -0400

    added card selection, and adding cards to deck

--- constants.gd
+++ constants.gd
@@ -2,8 +2,12 @@ extends Node
 
 enum PLAYERS {P1, P2}
 
+# used for deckbuilding ui
+enum SelectionArea { SELECTION, DECK, READY }
+
 const ELIXIR_COOLDOWN: float = 2.5
 const CURSOR_SPEED: float = 500
+const MAX_DECK_SIZE: int = 8
 const MAX_HAND_SIZE: int = 4
 const ACTION_COOLDOWN: float = 1.5
 
---
+++ ui/Deckbuilding_UI/ready_button.gd
@@ -0,0 +1,3 @@
+extends Node
+
+# contains highlight and unhighlight to keep consistent with other ui elements
