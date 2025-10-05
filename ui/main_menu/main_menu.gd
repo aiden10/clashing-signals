6e24497b242af5aeb6179e1faec613feaa6f71cb
aiden10 <aiden.ten30@gmail.com>
2025-10-05 12:52:15 -0400

    added main menu

---
+++ ui/main_menu/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var logo: TextureRect
+@export var start_button: Button
+
+func _ready() -> void:
+	start_button.pressed.connect(func(): GameStateManager.goto_scene(Constants.GAME_SCENES.DECK))
+	var tween = create_tween()
+	tween.set_loops()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	
+	tween.tween_property(logo, "modulate:a", 0.8, 2.0)
+	tween.tween_property(logo, "modulate:a", 1.0, 2.0)
