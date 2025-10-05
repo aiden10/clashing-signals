aed3fbf45444442ec5f3b7c50b46f6f565a989aa
Rainflight <rainflight24@gmail.com>
2025-10-05 13:28:36 -0400

    Improved modulation to skip modulating the elixir image

--- ui/card_display.gd
+++ ui/card_display.gd
@@ -6,10 +6,14 @@ class_name CardDisplay
 @onready var unhighlighted_panel: StyleBoxFlat = preload("res://ui/Unhighlight.tres")
 @onready var name_label: Label = $MarginContainer/VBoxContainer/CardName
 
+func _ready() -> void:
+	valid_selection()
+
 func setup(card_name: String, image: Texture, cost: int) -> void:
 	$MarginContainer/VBoxContainer/CardName.text = card_name
 	$MarginContainer/VBoxContainer/Image.texture = image
 	$MarginContainer/VBoxContainer/CenterContainer/ElixirCost.text = str(cost)
+	valid_selection()
 
 func is_empty() -> bool:
 	return $MarginContainer/VBoxContainer/CardName.text == "MISSING"
@@ -20,8 +24,16 @@ func highlight() -> void:
 func unhighlight() -> void:
 	self.add_theme_stylebox_override("panel", unhighlighted_panel)
 
+func selective_modulate(mod: Color) -> void:
+	self.self_modulate = mod
+	$MarginContainer.self_modulate = mod
+	$MarginContainer/VBoxContainer/CardName.modulate = mod
+
 func valid_selection() -> void:
-	self.modulate = Color(1, 1, 1, 1)
+	if is_empty():
+		self.selective_modulate(Color(18.892, 18.892, 0.0))
+	else:
+		self.selective_modulate(Color(1, 1, 1, 1))
 
 func invalid_selection() -> void:
-	self.modulate = Color(18.892, 0.0, 0.0)
+	self.selective_modulate(Color(18.892, 0.0, 0.0))
