5c12716718de2ab62b6f7542cbfab85cd3ef06a3
Rainflight <rainflight24@gmail.com>
2025-10-05 16:42:00 -0400

    increase cannonball lifespan

--- projectiles/boulder.gd
+++ projectiles/boulder.gd
@@ -4,3 +4,4 @@ func _init() -> void:
 	self.damage = Constants.CANNONBALL_DAMAGE
 	self.speed = Constants.CANNONBALL_SPEED
 	self.pierce_limit = Constants.CANNONBALL_PIERCE_LIMIT
+	self.max_lifespan = Constants.CANNONBALL_LIFESPAN
